bf5cb023b83cbbe54cf0a500e319c5cf2685bb45
Leo Ando <ando.reo420@gmail.com>
2021-08-21 01:00:07 +0900

    add simpleApp template

---
+++ SimpleApp/SimpleApp/FooViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SimpleApp.Core.ViewModel;
+
+namespace SimpleApp
+{
+    public class FooViewModel : ViewModelBase
+    {
+        string name;
+        public FooViewModel()
+        {
+        }
+
+        public string Name
+        {
+            set => SetProperty(ref name, value);
+            get => name;
+        }
+    }
+}
