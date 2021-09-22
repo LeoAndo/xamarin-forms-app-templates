fd4ff80562b734a1da91dc069139ec5383e39e89
LeoAndo <leo.777.info@gmail.com>
2021-09-22 22:06:43 +0900

    #4 IndicatorViewを使ったページング用のテンプレートを作成する

--- PagerSimpleApp/SimpleApp/App.xaml.cs
+++ PagerSimpleApp/SimpleApp/App.xaml.cs
@@ -7,9 +7,12 @@ namespace SimpleApp
         public App()
         {
             InitializeComponent();
+
+
+            Color barBackgroundColor = (Device.RuntimePlatform == Device.Android) ? Color.Black : Color.Orange;
             MainPage = new NavigationPage(new MainPage())
             {
-                BarBackgroundColor = Color.FromHex("#000")
+                BarBackgroundColor = barBackgroundColor
             };
         }
         protected override void OnStart()
