9e60fca82e2917096480662f3e8ce8a824ee46ea
LeoAndo <leo.777.info@gmail.com>
2021-09-22 21:33:53 +0900

    #4 IndicatorViewを使ったページング用のテンプレートを作成する

---
+++ PagerSimpleApp/SimpleApp/FooViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using SimpleApp.Models;
+using Xamarin.Forms;
+
+namespace SimpleApp
+{
+    public class FooViewModel
+    {
+        // ObservableCollection: 内部でINotifyPropertyChangedを実装している.
+        public ObservableCollection<Person> Persons { get; private set; }
+        public FooViewModel()
+        {
+            CreateCollection();
+        }
+
+        void CreateCollection()
+        {
+            var source = new List<Person>
+            {
+
+                new Person
+                {
+                    Name = "Yamada",
+                    Details = "Details...........",
+                    ImageSource = ImageSource.FromResource("SimpleApp.Images.sample_image1.png")
+                },
+
+                new Person
+                {
+                    Name = "Tanaka",
+                    Details = "Details...........",
+                    ImageSource = ImageSource.FromResource("SimpleApp.Images.sample_image2.png")
+                },
+
+                new Person
+                {
+                    Name = "Yoshida",
+                    Details = "Details...........",
+                    ImageSource = ImageSource.FromResource("SimpleApp.Images.sample_image3.png")
+                },
+            };
+            Persons = new ObservableCollection<Person>(source);
+        }
+    }
+}
---
+++ PagerSimpleApp/SimpleApp/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace SimpleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void CarouselView_PositionChanged(System.Object sender, Xamarin.Forms.PositionChangedEventArgs e)
+        {
+            currentPos.Text = $"CurrentPageNo: {e.CurrentPosition + 1}";
+        }
+    }
+}
