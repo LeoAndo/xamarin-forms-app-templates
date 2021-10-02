3601e69465c690e2e992bcf268fae782aaa20f3e
LeoAndo <leo.777.info@gmail.com>
2021-10-02 22:38:43 +0900

    add template app

---
+++ SharedTransitionPagerApp/SimpleApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace SimpleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+
+            Color barBackgroundColor = (Device.RuntimePlatform == Device.Android) ? Color.Black : Color.Orange;
+            MainPage = new NavigationPage(new MainPage())
+            {
+                BarBackgroundColor = barBackgroundColor
+            };
+        }
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
